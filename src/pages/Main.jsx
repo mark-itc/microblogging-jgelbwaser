f5d4f346b4a8e1e34b3de5bd19fe7e9e2fc5b141
jgelbwaser <99264301+jgelbwaser@users.noreply.github.com>
2022-11-21 11:51:41 +0200

    M1 completed

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,8 @@
 import './Main.css';
 import CreateTweet from '../components/CreateTweet';
 import Tweet from '../components/Tweet';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import localForage from 'localforage';
 
 
 function Main() {
@@ -13,34 +13,44 @@ function Main() {
     //const date = new Date
 
     const addTweet = (tweetTxt) => {
-        const tweet ={
+        const tweet = {
             content: tweetTxt,
             userNAme: username,
             date: new Date().toISOString(),
         }
-        console.log(tweet);
         const newTweetArray = [...tweets];
-       newTweetArray.unshift(tweet);
-       setTweets(newTweetArray);
+        newTweetArray.unshift(tweet);
+        setTweets(newTweetArray);
+        localForage.setItem('tweets', newTweetArray);
     }
 
+    const getOfflineTweets = async () => {
+        const offlineTweets = await localForage.getItem('tweets');
+        console.log('offlineTweets', offlineTweets);
+        offlineTweets && setTweets(offlineTweets);
+    }
 
-    return (
-        <>
-            <CreateTweet processSubmit={addTweet} />
-            <div className='tweet-list'>
-                {tweets.map(({content, date}, index)=>{
-                     return <Tweet 
-                        key = {index}
-                        userName = {username}
-                        content = {content}
-                        date = {date} />
-                })}
-            </div>
-        </>
+    useEffect(() => {
+        getOfflineTweets(); 
+    }, []);
 
 
-    )
+return (
+    <>
+        <CreateTweet processSubmit={addTweet} />
+        <div className='tweet-list'>
+            {tweets.map(({content, date}, index) => {
+                return <Tweet
+                    key={index}
+                    userName={username}
+                    content={content}
+                    date={date} />
+            })}
+        </div>
+    </>
+
+
+)
 }
 
 export default Main;
